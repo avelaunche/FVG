d3eb1a3e260d47b73bf75b5e65c6fdd0925a79fb
Kaixiang Loke <kaixiangloke@Kaixiangs-MacBook-Air-3.local>
2025-11-06 15:45:36 -0500

    General Commit

--- Analyze Function.R
+++ Analyze Function.R
@@ -1,38 +1,42 @@
-analyze_function_bull_fvg <- function(hi){
+analyze_function_bear_fvg <- function(hi, day1){
   high = hi$high
   number = hi$number
   hi$number
-  l = (number+1):nrow(day1)
-  g = day1[l,]
-  a = high > g$low
-  ab = g[a,]
+  end = (number+1):nrow(day1)
+  end_of_day = day1[end,]
+  end_true = high > end_of_day$low
+  ab = end_of_day[end_true,]
   nrow(ab)
-  if (nrow(ab) == 0){
+  if (nrow(ab) == 0 | nrow(ab) == 1){
     print("A")
     return(FALSE)
   }
-  IMPORTANT = ab[1,]
-  IMPORTANT
-
+  ab
+  IMPORTANT = ab[2,]
+  
   day1left = day1[IMPORTANT$number:nrow(day1),]
   break_through = day1left$timestamp[day1left$low < hi$low]
   break_through
-  break_above = day1left$timestamp[day1left$high > hi$high]
-  break_above = break_above[2:length(break_above)]
-  print(break_through)
-  print(break_above)
   
-  print("HIODAHODA")
+  diff = hi$close - hi$open
   
-  print(break_through[1])
-  print(break_above[1])
-
-  if (length(x2) == 0){
-    print("B")
+  break_above = day1left$timestamp[day1left$high > (hi$high)]
+  break_above
+  break_above = break_above[1:length(break_above)]
+  
+  break_above = break_above[!is.na(break_above)]
+  
+  diff = day1$close - day1$open
+  
+  if (length(break_through) == 0){
+    print("A")
     return(TRUE)
   }
   
-  
+  if (length(break_above) == 0){
+    print("A")
+    return(FALSE)
+  }
   
   if (break_through[1] < break_above[1]) {
     print("C")
@@ -45,7 +49,8 @@ analyze_function_bull_fvg <- function(hi){
 
 
 
-analyze_function_bear_fvg <- function(hi){
+
+analyze_function_bear_fvg <- function(hi, day1){
   low = hi$low
   number = hi$number
   hi
@@ -73,6 +78,7 @@ analyze_function_bear_fvg <- function(hi){
     print("A")
     return(FALSE)
   }
+
   break_below = break_below[2:length(break_below)]
   print(break_through)
   print(break_below)
